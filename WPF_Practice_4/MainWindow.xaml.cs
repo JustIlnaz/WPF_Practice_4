85818a035cc4deb808011130c1c8fa800c43938b
Ilnaz <Ilnazikwork@gmail.com>
2025-04-18 12:24:23 +0300

    fixed bugs

--- WPF_Practice_4/MainWindow.xaml.cs
+++ WPF_Practice_4/MainWindow.xaml.cs
@@ -1,28 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WPF_Practice_4
+namespace Calculator
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string currentInput = ""; // Текущий ввод пользователя
+        private string lastOperation = ""; // Последняя операция
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Обработчик нажатия на цифровую кнопку
+        private void DigitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            currentInput += btn.Tag.ToString();
+            ResultTextBox.Text = currentInput;
+        }
+
+        // Обработчик нажатия на операционную кнопку
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            lastOperation = btn.Tag.ToString();
+            currentInput += " " + lastOperation + " ";
+            ResultTextBox.Text = currentInput;
+        }
+
+        // Обработчик нажатия на кнопку "="
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double result = EvaluateExpression(currentInput);
+                ResultTextBox.Text = result.ToString();
+                currentInput = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка вычисления: " + ex.Message);
+            }
+        }
+
+        // Обработчик нажатия на кнопку "C" (очистка)
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentInput = "";
+            lastOperation = "";
+            ResultTextBox.Text = "0";
+        }
+
+        // Метод для вычисления выражения
+        private double EvaluateExpression(string expression)
+        {
+            try
+            {
+                // Используем простой парсинг и вычисление
+                var dataTable = new System.Data.DataTable();
+                var result = dataTable.Compute(expression, null);
+                return Convert.ToDouble(result);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Неверное выражение");
+            }
         }
     }
 }
